60eb7027ffe377f80a2129d39b587427c0c34305
adriana_appdev <abritoru@chicagobooth.edu>
2019-05-03 20:24:45 +0000

    User#own_photos

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,5 +12,13 @@
 #
 
 class User < ApplicationRecord
-    
+    def comments
+    return Comment.where({:author_id => self.id})
+    end 
+    #I have troubble applying this method. I have defined B as the User.where ID = 81. When I do B. comments it gives me error but the program says it is correct"
+
+    def own_photos
+        return Photo.where({:owner_id => self.id})
+    end 
+    # P = photo.first, when I apply the method I get a mistake here# 
 end
